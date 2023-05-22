a244434ecf7e710f6ab3dba0e31a94fce2733ffa
chrisbdj <me@chrisj.tech>
2023-05-22 16:06:47 -0600

    Removed unneccessary code and comments

--- chessboard.py
+++ chessboard.py
@@ -1,10 +1,4 @@
-#!/usr/bin/env python
-
 from __future__ import print_function
-
-# Read button input through an SN74LS165 shift register.
-# Adapted from the Arduino version at
-# https://playground.arduino.cc/Code/ShiftRegSN74HC165N
 
 import RPi.GPIO as GPIO
 import time
@@ -37,16 +31,14 @@ class SN74LS165:
         GPIO.setup(self.clock_enable, GPIO.OUT)
 
     def read_shift_regs(self):
-        # Logic copied from the Arduino program.
-        # XXX Doesn't make room fo data width for higher numbers of chips.
+        # Trigger a parallel Load to latch the state of the data lines,
         GPIO.output(self.clock_enable, GPIO.HIGH)
         GPIO.output(self.latch, GPIO.LOW)
         time.sleep(SN74LS165.pulse_time)
         GPIO.output(self.latch, GPIO.HIGH)
         GPIO.output(self.clock_enable, GPIO.LOW)
-
-        #bytes_val = 0
         arr = []
+        # Loop to read each bit value from the serial out line of the SN74HC165N.
         for i in range(self.datawidth):
             bit = GPIO.input(self.data)
             
@@ -59,7 +51,6 @@ class SN74LS165:
                     
             
             arr.append(bit)
-            #arr.append(i+1)
             # Pulse the clock: rising edge shifts the next bit.
             GPIO.output(self.clock, GPIO.HIGH)
             time.sleep(SN74LS165.pulse_time)
@@ -73,25 +64,12 @@ if __name__ == '__main__':
     # Use GPIO numbering:
     GPIO.setmode(GPIO.BCM)
 
-    # Python has no native way to print binary unsigned numbers. Lame!
-    def tobin(data, width=8):
-        data_str = bin(data & (2**width-1))[2:].zfill(width)
-        return data_str
-
     shiftr = SN74LS165(clock=11, latch=7, data=9, clock_enable=8, num_chips=8)
-    # shiftr = SN74LS165(clock=22, latch=23, data=17, clock_enable=27, num_chips=8)
     try:
         while True:
-            # b1 = shiftr.read_one_byte()
-            # print(format(b1, '#010b'))
-            # bytes = [ shiftr.read_one_byte() ]
             bytes = shiftr.read_shift_regs()
-            
-            #print('   '.join([tobin(b, 8) for b in bytes]))
-            #print('   '.join(["%8x" % b for b in bytes]))
             print(bytes)
             print("")
-            # Sleep at least .0001 between reads
             time.sleep(0.01)
     except KeyboardInterrupt:
         GPIO.cleanup()
