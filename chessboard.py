55f840607550871b00811e4aa12b3bb71fe3dd79
Chris Jorgensen <me@chrisj.tech>
2023-06-04 22:39:58 -0600

    .

--- chessboard.py
+++ chessboard.py
@@ -220,7 +220,7 @@ def updateBoard(boardArr, updatedBoardArr):
                     led = convertCoordToLED(possible_moves[j])
                     updateLED(led,1)
                 
-                possible_moves = []
+                #possible_moves = []
             else:
                 # Item not found in the list
                 print(coord," not found in actively raised pieces? maybe this is a take? ")
